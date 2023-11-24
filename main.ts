c4079b3314dd3edd70c09625b37e246726efadec
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2023-11-24 11:12:22 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+// Capture the input of button a pressing
+input.onButtonPressed(Button.A, function () {
+    // wait 5 secs
+    control.waitMicros(0.5 * MICROSECOND_PER_SECOND)
+    pins.servoWritePin(AnalogPin.P1, P1_FORWARD)
+    pins.servoWritePin(AnalogPin.P2, P2_FORWARD)
+    control.waitMicros(10 * MICROSECOND_PER_SECOND)
+    pins.servoWritePin(AnalogPin.P1, P1_STOP)
+    pins.servoWritePin(AnalogPin.P2, P2_STOP)
+})
+let P2_STOP = 0
+let P1_STOP = 0
+let P2_FORWARD = 0
+let P1_FORWARD = 0
+let MICROSECOND_PER_SECOND = 0
+// ===== Unchengeable constants =====
+MICROSECOND_PER_SECOND = 1000000
+// ===== Individual buggy constants =====
+// you should know this from the forward calibrator
+P1_FORWARD = 71
+P2_FORWARD = 180
+let DEGREES_PER_SECOND = 251
+// ===== Individual buggy constants =====
+// you should know this from the forward calibrator
+P1_STOP = 90
+P2_STOP = 90
