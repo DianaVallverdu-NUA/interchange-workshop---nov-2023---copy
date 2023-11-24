97c512b465ce12fbeb982d8b883a0b2c34f6db43
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2023-11-24 11:13:45 +0000

    Separate move forward to a different function

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 // Capture the input of button a pressing
 input.onButtonPressed(Button.A, function () {
+	
+})
+function move_forward_for_5_seconds () {
     // wait 5 secs
     control.waitMicros(0.5 * MICROSECOND_PER_SECOND)
     pins.servoWritePin(AnalogPin.P1, P1_FORWARD)
     pins.servoWritePin(AnalogPin.P2, P2_FORWARD)
-    control.waitMicros(10 * MICROSECOND_PER_SECOND)
+    control.waitMicros(5 * MICROSECOND_PER_SECOND)
     pins.servoWritePin(AnalogPin.P1, P1_STOP)
     pins.servoWritePin(AnalogPin.P2, P2_STOP)
-})
+}
 let P2_STOP = 0
 let P1_STOP = 0
 let P2_FORWARD = 0
